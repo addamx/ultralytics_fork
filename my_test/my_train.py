e45cb43f7bc6c564d9003f491034c78ebee5b39d
Addamx <haoranclover@gmail.com>
2024-10-23 22:56:07 +0800

    v

--- my_test/my_train.py
+++ my_test/my_train.py
@@ -7,25 +7,52 @@ if __name__ == "__main__":
     '''
     yolo/model.py(engine/model.py#Model)
     0. __init__
-    self.callbacks = callbacks.get_default_callbacks() # 参考 ultralytics/utils/callbacks/base.py#default_callbacks, 包含trainer, validator, predictor, exporter的各阶段的hook
+    self.callbacks = callbacks.get_default_callbacks() # 参考 ultralytics/utils/callbacks/base.py default_callbacks, 包含trainer, validator, predictor, exporter的各阶段的hook
 
     1. _load
-    > attempt_load_one_weight(yolo11n.pt)
-    >> ckpt = torch_safe_load(yolo11n.pt)
-    >> model = ckpt.model # DetectionModel 序列化的nn.Module
-    >> 超参数 model.args = {**DEFAULT_CFG_DICT, **(ckpt.get("train_args", {}))} 
-    
+    attempt_load_one_weight(yolo11n.pt) # 获取 model(DetectionModel) 和 ckpt
+        ckpt = torch_safe_load(yolo11n.pt)
+        model = ckpt.model # DetectionModel
+    model.args = {**DEFAULT_CFG_DICT, **(ckpt.get("train_args", {}))}  超参数
     model.yaml 对应 yolo11n.yaml，记录了backbone、head、nc(80)、scale(n)、ch(3)
     '''
     model = YOLO("yolo11n.pt")
 
     # 训练模型
     '''
-    engine/model.py
-    1. train
-    > self.trainer = _smart_load("trainer")
-    > yolo.detect.DetectionTrainer(engine/trainer.py#BaseTrainer) __init__
-    >> trainer.model = get_model(self.model)
+    engine/model.py train
+    1. self.trainer = _smart_load("trainer") # DetectionTrainer(BaseTrainer)
+    trainer.model = self.trainer.get_model(self.model) #nn/task.py DetectionModel
+    DetectionModel.load(self.model) 从序列化模型中提取weights
+    
+    
+    DetectionTrainer(BaseTrainer)
+    2. self.trainer.train() # BaseTrainer.train()
+    world_size > 0: (多卡)
+        generate_ddp_command -> torch.distributed.launch
+    else: 单卡
+        self._do_train(）
+        
+    3. self._do_train(）
+    self._setup_train(world_size) 
+    
+    3.1 self._setup_train
+    set_model_attributes
+        model.nc    # 类数目
+        model.names # 类别名
+        model.args # 超参数
+    Freeze layers
+    amp模式（模型权重和激活使用 16 位精度存储，但某些计算保持 32 位精度，以保持数值稳定性）
+        self.amp, self.scaler
+    self.stride = 32
+    check_train_batch_size
+    self.train_loader = self.get_dataloader
+    
+    3.1.1 get_dataloader
+    build_dataset 
+        build_yolo_dataset
+    
+        
     '''
     train_results = model.train(
         data="./1848371414130941954.yaml",  # 数据集 YAML 路径
